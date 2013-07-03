421b7f7a81b2638b776ace7f13419438ca7f17f5
david <david@david-lt>
2013-07-03 21:53:16 +0100

    att fix

--- set_up_files.sh
+++ set_up_files.sh
@@ -7,5 +7,5 @@ mv ~/.bashrc  ~/.bashrc_old
 
 
 #create sym link to synced files
-ln -s config_repos/.tmux.conf ~/.tmux.conf 
-ln -s config_repos/.bashrc ~/.bashrc   
+ln -s ~/config_repos/.tmux.conf ~/.tmux.conf 
+ln -s ~/config_repos/.bashrc ~/.bashrc   
