f325604dda14c6f906d9d44a0f660dc8333db2c2
David O Shea <osheadavid7@gmail.com>
2013-11-18 22:56:22 +0000

    added better check_path that checks a file/folder and moves individually, rather than once all files are synced. also fixed cron job that will now run python script

--- check_db.py
+++ check_db.py
@@ -3,17 +3,13 @@ import logging
 import sys
 from subprocess import call
 sys.path.append('./') #for dropbox.py and settings.py
-sys.stdout = open('example.log', 'a')
+sys.stdout = open('mylog.log', 'a')
 from settings import lnk,ulk,dwd
 import os
 from time import gmtime, strftime
 
 def tn(): return "["+strftime("%Y-%m-%d %H:%M:%S", gmtime())+"] " 
 log=open('mylog.log','a')
-
-#set log file
-#logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='example.log',level=logging.DEBUG)
-
 
 def check_wd(dwd):
 #check wd
@@ -23,40 +19,29 @@ def check_wd(dwd):
     #set wd
         os.chdir(dwd)
     
-
 def check_path(lnk,ulk):
-    #check if dir is up yo date
-    status=call(["./dropbox.py","filestatus",lnk])
-    #
-    if status==0:
-    #look for folders
- #       logging.info('filestatus == %s (synced)',str(status))
-        log.write(tn()+' filestatus is %s (synced) \n' %str(status))
-        p,dirs,files=os.walk(lnk).next()
-    #if there are subfolders, move them
-        if len(dirs)!=0:
-#            logging.info('Found %s folders',len(dirs))
-            log.write(tn()+'Found %s folders \n' %len(dirs))
-            for dr in dirs:
-#                logging.info('Moving %s ...',str(dr))
+    p,dirs,files=os.walk(lnk).next()
+    if len(dirs)!=0:
+        log.write(tn()+' Found %s folders \n' %len(dirs))
+        for dr in dirs:
+            status=call(["./dropbox.py","filestatus",dr])
+            if status==1:
+            #Move synced folders
                 log.write(tn()+'Moving %s ... \n' %str(dr))
                 call(["mv",lnk+'/'+dr,ulk+'/'+dr])
-        else:
-#            logging.info('No folders found')
-            log.write(tn()+' No folders found \n')
-        if len(files)!=0:
-#            logging.info('Found %s files',len(files))
-            log.write(tn()+' Fould %s files' %len(files))
-            for f in files:
-            #    logging.info('Moving %s ...',str(f))
+    else:
+        log.write(tn()+' No folders found \n')
+    if len(files)!=0:
+        log.write(tn()+' Fould %s files' %len(files))
+        for f in files:
+            status=call(["./dropbox.py","filestatus",f])
+            if status==1:
                 log.write('Moving %s ...\n' %str(f))
                 call(["mv",lnk+'/'+f,ulk+'/'+f])
-        else:
-            log.write(tn()+' No files found\n')
     else:
-    #Not yet synced
-        log.write(tn()+' Not yet synced, try again later\n')
+        log.write(tn()+' No files found\n')
     log.close()
+
 
 def do_all(lnk,ulk,dwd):
     check_wd(dwd)
@@ -66,4 +51,4 @@ def do_all(lnk,ulk,dwd):
         
 if __name__ =='__main__':
     do_all(lnk,ulk,dwd)
-    print 'script has run'
+    print tn()+' script has run'
