7cd46c33de3830c9faf5df51028eea920d347f2c
David O Shea <osheadavid7@gmail.com>
2014-01-04 20:48:27 +0000

    major changes

--- check_db.py
+++ check_db.py
@@ -3,17 +3,26 @@ import time
 from subprocess import check_call, check_output,call
 import subprocess
 import logging
+import os
+
 from time import gmtime, strftime
 def tn(): return "["+strftime("%Y-%m-%d %H:%M:%S", gmtime())+"] "
 
+import os
+
 log=open('mylog.log', 'a')
-DIR='Dropbox/Sean n David/For David/'
-REMOTE_DIR = 'Unsorted2/'
+DIR='/media/david/4ee8607d-eb37-4373-85f1-97e1d8bd0fbb/Dropbox/Sean n David/For David/' 
+REMOTE_DIR = '/media/david/4ee8607d-eb37-4373-85f1-97e1d8bd0fbb/Unsorted2/'
 
 if check_output(['./dropbox.py', "status"]) == "Idle\n":
     log.write(tn()+'Moving files....\n')
-    subprocess.Popen(["mv",DIR,REMOTE_DIR])
-    subprocess.Popen(["mkdir",DIR])
+    subprocess.call(['./move_folders.sh'])
+    '''
+    listoffiles=os.listdir(DIR)                                       
+    for f in listoffiles:                                             
+        fullpath=DIR+f                                            
+        subprocess.Popen('mv'+' '+fullpath+' '+REMOTE_DIR,shell=True) 
+    '''
 else:
     log.write(tn()+'Not synced....\n')
 
