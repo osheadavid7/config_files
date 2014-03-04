9a43136ef7a7693b180d4e614211b9a6e515e12a
david <david.oshea@tyndall.ie>
2014-03-04 12:01:32 +0000

    added git-wip and changes sym path, sym link must exist in /bin/ to git-wip* in order to work

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;------------Set package archives---------------------
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
@@ -10,17 +11,15 @@
                ac-source-words-in-same-mode-buffers
                ac-source-semantic))
 
+;---------------------required packages
 (load "auctex.el" nil t t)
-(add-to-list 'load-path "/home/david/clones/config_files/emacs_packages/")
-;(add-to-list 'load-path "/home/david/clones/config_files/emacs_packages/auto-complete-auctex.el")
-(add-to-list 'load-path "/home/david/clones/config_files/emacs_packages/auto-complete-1.3.1/")
 (load "preview-latex.el" nil t t)
-(setq TeX-PDF-mode t)
-(setq TeX-view-program-selection
-      '((output-pdf "PDF Viewer")))
-(setq TeX-view-program-list
-      '(("PDF Viewer" "okular %o")))
-(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+
+(add-to-list 'load-path "~/clones/config_files/emacs_packages/")
+(add-to-list 'load-path "~/clones/config_files/emacs_packages/auto-complete-1.3.1/")
+
+;---------------------add git wip
+(load "~/clones/config_files/git-wip/emacs/git-wip.el")
 
 
 (require 'package)
@@ -31,8 +30,18 @@
 (require 'auto-complete-auctex)
 
 (ac-config-default)
-
 (require 'ac-math) 
+
+(setq ac-math-unicode-in-math-p t)
+(setq TeX-PDF-mode t)
+(setq TeX-view-program-selection
+      '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+      '(("PDF Viewer" "okular %o")))
+
+(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+
+
 (add-to-list 'ac-modes 'latex-mode)   ; make auto-complete aware of `latex-mode`
 
  (defun ac-LaTeX-mode-setup () ; add ac-sources to default ac-sources
@@ -43,5 +52,4 @@
 (add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
 (global-auto-complete-mode t)
  
-(setq ac-math-unicode-in-math-p t)
 
