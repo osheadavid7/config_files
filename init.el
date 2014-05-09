dcf2bca99984508656f5be5ed873d80c3b10f6f0
David O Shea <osheadavid7@gmail.com>
2014-05-09 19:36:10 +0100

    updated and reorganised init.el

--- init.el
+++ init.el
@@ -15,24 +15,12 @@
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
 
-(add-to-list 'load-path "~/clones/config_files/emacs_packages/")
-(add-to-list 'load-path "~/clones/config_files/emacs_packages/auto-complete-1.3.1/")
 
-
-(setq x-select-enable-clipboard t) ; as above
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 ;---------------------add git wip
 (load "~/clones/config_files/git-wip/emacs/git-wip.el")
 (package-initialize)
 
 
-;For some reason, these have to go here. After loading more packages they get forgotten!!
-(setq TeX-PDF-mode t)
-(setq TeX-view-program-selection
-      '((output-pdf "PDF Viewer")))
-(setq TeX-view-program-list
-      '(("PDF Viewer" "okular %o")))
-(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
 
 (require 'reftex)
 (require 'package)
@@ -42,8 +30,36 @@
 (ac-config-default)
 (require 'ac-math) 
 
+(setq ispell-dictionary "british") 
 (setq ac-math-unicode-in-math-p t)
 (setq reftex-plug-into-AUCTeX t)
+
+(setq TeX-PDF-mode t)
+(setq TeX-view-program-selection
+      '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+      '(("PDF Viewer" "okular %o")))
+
+
+
+(setq reftex-default-bibliography '("/home/david/clones/latex_bibs/dave.bib"))
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
+(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+
+(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
+
+
+(defun ac-LaTeX-mode-setup () ; add ac-sources to default ac-sources
+  (setq ac-sources
+	(append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands)
+		ac-sources))
+  )
+
+;(add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
+;(global-auto-complete-mode t)
+
+(add-to-list 'ac-modes 'latex-mode)   ; make auto-complete aware of `latex-mode`
 
 ;Auto complete from Andreas
 (global-set-key "\M-/" (make-hippie-expand-function
@@ -53,21 +69,10 @@
                                 try-complete-file-name-partially
                                 try-complete-file-name) t))
 
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
-(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
 
-(add-to-list 'ac-modes 'latex-mode)   ; make auto-complete aware of `latex-mode`
 
- (defun ac-LaTeX-mode-setup () ; add ac-sources to default ac-sources
-   (setq ac-sources
-         (append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands)
-                 ac-sources))
-   )
-;(add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
-;(global-auto-complete-mode t)
 
-(setq ispell-dictionary "british") 
-(setq reftex-default-bibliography '("/home/david/clones/latex_bibs/dave.bib"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
